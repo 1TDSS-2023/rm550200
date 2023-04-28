a81f3521fc83782bba8ca3a2dc15eaa52869a67f
EsparzaPhoenix <esparza_leonardo@hotmail.com>
2023-04-28 20:59:55 -0300

    imagem arvore dom

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+//console.log("ola mundo");
+
+var nr1 = "10"
+var nr2 = "5"
+
+//Realizando operações com Strings
+//console.log(parseInt( nr1)+parseInt(nr2));
+
+var nome1 = "dubas";
+//let nome2 = "serbatinho";
+//const nome3 = "Julia"
+
+if(nome1 != ""){
+    var nome1 = "julia";
+}
+
+console.log(nome1);
